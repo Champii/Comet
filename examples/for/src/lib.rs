e3e629b8d866d94d82139687409aa2fd9f3ae3d9
Champii <contact@champii.io>
2022-10-22 05:40:34 +0200

    Add a for example

---
+++ examples/for/src/lib.rs
@@ -0,0 +1,15 @@
+use comet::prelude::*;
+
+component! {
+    i32,
+    div {
+        for ((i) in 0..*self) {
+            button {
+                { i }
+            }
+        }
+
+    }
+}
+
+comet!(10);
