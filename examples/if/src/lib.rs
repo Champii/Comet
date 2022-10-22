24df24ec875b8f1d4af3b0d7f0f178eade064d40
Champii <contact@champii.io>
2022-10-22 05:37:14 +0200

    Add if example

---
+++ examples/if/src/lib.rs
@@ -0,0 +1,22 @@
+use comet::prelude::*;
+
+#[derive(Default)]
+pub struct Toggle {
+    show: bool,
+}
+
+component! {
+    Toggle,
+    div {
+        button @click: { self.show = !self.show } {
+            { if self.show { "Visible" } else { "Hidden" } }
+        }
+        if (self.show) {
+            div {
+                { "This is visible" }
+            }
+        }
+    }
+}
+
+comet!(Toggle::default());
